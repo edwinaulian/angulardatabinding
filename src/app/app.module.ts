b4df7dc94b781df548af63b83c61a62423a0d4c1
edwin <edwinauliano@gmail.com>
2018-09-18 16:50:33 +0700

    all commit for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostComponent } from './post/post.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PostComponent,
+    UsersComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { trigger, style, transition, animate, keyframes, query, stagger  } from '@angular/animations';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(-15px)'}),
+          stagger('50ms',
+          animate('550ms ease-out',
+          style({
+            opacity: 1, transform: 'translateY(0px)'
+          })))
+        ], { optional: true}),
+        query(':leave', animate('50ms', style ({opacity: 0 })), { optional: true})
+      ])
+    ])
+]
+})
+export class PostComponent implements OnInit {
+
+  posts$: Object;
+
+  constructor(private data: DataService) { 
+  
+  }
+
+  ngOnInit() {
+    this.data.getPosts().subscribe(
+      data => this.posts$ = data
+    )
+  }
+
+}
